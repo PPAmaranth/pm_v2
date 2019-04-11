76deb49d7a26ad6fa94d2addf24f5c86849b178c
PPAmaranth <523002128@qq.com>
2019-04-11 09:18:36 +0800

    跨域报错

--- src/main/java/pp/pokemon/pm/web/controller/book/BookController.java
+++ src/main/java/pp/pokemon/pm/web/controller/book/BookController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import pp.pokemon.pm.service.book.BookService;
+import pp.pokemon.pm.service.book.impl.BookServiceImpl;
 import pp.pokemon.pm.web.controller.BaseController;
 import pp.pokemon.pm.web.vo.base.DefaultApiResult;
 import pp.pokemon.pm.web.vo.book.GetBookReqVo;
@@ -15,7 +15,7 @@ import pp.pokemon.pm.web.vo.book.GetBookReqVo;
 public class BookController extends BaseController {
 
     @Autowired
-    private BookService bookService;
+    private BookServiceImpl bookService;
 
     @RequestMapping(value = "/getBookById", method = {RequestMethod.POST})
     public DefaultApiResult getBookById(@RequestBody GetBookReqVo reqVo) {
--- src/main/java/pp/pokemon/pm/web/vo/book/GetBookReqVo.java
+++ src/main/java/pp/pokemon/pm/web/vo/book/GetBookReqVo.java
@@ -5,5 +5,6 @@ import lombok.Data;
 @Data
 public class GetBookReqVo {
 
-    private Integer id;
+    private int id;
+
 }
